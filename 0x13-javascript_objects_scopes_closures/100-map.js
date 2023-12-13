30824d7e60a7eb8db9c657e5da902857b16649a6
Ikechukwu Faithful <132079715+JayJay247in@users.noreply.github.com>
2023-12-13 09:36:30 +0100

    Updated 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
-#!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const list = require('./100-data').list;
+
+const newList = list.map((val, index) => {
+  return val * index;
+});
+
+console.log(list); 
+console.log(newList);
